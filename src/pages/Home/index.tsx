c95a9072a8d766c65d29e210c269b9626ebda86e
hnascx <henriquehusky@gmail.com>
2023-07-12 23:30:19 -0300

    (6) Still working in banner at Home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,54 @@
 import {
   BannerContainerBackground,
   BannerContainer,
-  BannerContainerTexts,
+  BannerContainerElements,
+  BannerContainerTitle,
 } from './styles'
 
 export function Home() {
   return (
     <BannerContainer>
       <BannerContainerBackground />
-      <BannerContainerTexts>
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-      </BannerContainerTexts>
-      <img src="" alt="" />
+      <BannerContainerElements>
+        <BannerContainerTitle>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe o seu café onde estiver, a
+            qualquer hora!
+          </p>
+          <div>
+            <span>
+              <img
+                src="https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png"
+                width={24}
+              />
+              <div>Compra simples e segura</div>
+            </span>
+            <span>
+              <img
+                src="https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png"
+                width={24}
+              />
+              <div>Embalagem mantém o café intacto</div>
+            </span>
+            <span>
+              <img
+                src="https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png"
+                width={24}
+              />
+              <div>Entrega rápida e rastreada</div>
+            </span>
+            <span>
+              <img
+                src="https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png"
+                width={24}
+              />
+              <div>O café chega fresquinho até você</div>
+            </span>
+          </div>
+        </BannerContainerTitle>
+        <div>imagem</div>
+      </BannerContainerElements>
     </BannerContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,21 +6,52 @@ export const BannerContainer = styled.div`
   align-items: center;
   justify-content: center;
 
-  width: 90rem;
-  height: 34rem;
+  max-width: 90rem;
 `
 export const BannerContainerBackground = styled.main`
   position: absolute;
+
   width: 90rem;
   height: 34rem;
+
   background-image: url(${backgroundBanner});
   filter: blur(80px);
 `
-export const BannerContainerTexts = styled.section`
+export const BannerContainerElements = styled.div`
   width: 70rem;
   height: 34rem;
-  padding-top: 5.875rem;
+  padding: 5.875rem 0 6.75rem;
+
   display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
+`
+
+export const BannerContainerTitle = styled.section`
+  max-width: 36.75rem;
+
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 3rem;
+    line-height: 1.3;
+    font-weight: 800;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.25rem;
+    line-height: 1.3;
+    font-weight: 400;
+  }
+
+  div {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `
