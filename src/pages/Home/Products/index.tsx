db46d3ffe7b4dbe0baedf795a4c12c03ca1c2bef
hnascx <henriquehusky@gmail.com>
2023-07-19 21:03:34 -0300

    (11) Working at Products list at Products component

--- src/pages/Home/Products/index.tsx
+++ src/pages/Home/Products/index.tsx
@@ -4,6 +4,13 @@ import {
   ProductsSectionTitle,
   ProductContainer,
   ProductCard,
+  ProductCardImage,
+  ProductCardInfos,
+  ProductCardCoffeeType,
+  ProductCardCoffeeName,
+  ProductCardCoffeeDescription,
+  ProductCardCurrency,
+  ProductCardPrice,
 } from './styles'
 
 export function Products() {
@@ -12,7 +19,17 @@ export function Products() {
       <ProductsSectionTitle>Nossos cafés</ProductsSectionTitle>
       <ProductContainer>
         <ProductCard>
-          <img src={expressoTradicional} alt="" />
+          <ProductCardImage src={expressoTradicional} alt="" />
+          <ProductCardInfos>
+            <ProductCardCoffeeType>Tradicional</ProductCardCoffeeType>
+            <ProductCardCoffeeName>Expresso Tradicional</ProductCardCoffeeName>
+            <ProductCardCoffeeDescription>
+              O tradicional café feito com água quente e grãos moídos
+            </ProductCardCoffeeDescription>
+            <ProductCardCurrency>
+              R$<ProductCardPrice>9,90</ProductCardPrice>
+            </ProductCardCurrency>
+          </ProductCardInfos>
         </ProductCard>
       </ProductContainer>
     </ProductsContainerSection>
--- src/pages/Home/Products/styles.ts
+++ src/pages/Home/Products/styles.ts
@@ -22,19 +22,87 @@ export const ProductContainer = styled.div`
 
 export const ProductCard = styled.div`
   display: flex;
-  align-items: flex-start;
   justify-content: center;
   width: 16rem;
   height: 19.375rem;
+
+  padding: 1.25rem;
 
   background: ${(props) => props.theme['base-card']};
 
   border-radius: 6px 36px;
 
   position: relative;
+`
 
-  img {
-    position: absolute;
-    bottom: 13.25rem;
-  }
+export const ProductCardImage = styled.img`
+  display: flex;
+  justify-content: center;
+
+  position: absolute;
+  left: 4rem;
+  bottom: 13.25rem;
+`
+
+export const ProductCardInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 5.75rem;
+`
+
+export const ProductCardCoffeeType = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Roboto', sans-serif;
+  font-size: 0.625rem;
+  font-weight: 700;
+  line-height: 1.3;
+  text-transform: uppercase;
+
+  padding: 0.25rem 0.5rem;
+  border-radius: 100px;
+  margin-bottom: 1rem;
+
+  background: ${(props) => props.theme['yellow-light']};
+  color: ${(props) => props.theme['yellow-dark']};
+`
+export const ProductCardCoffeeName = styled.h3`
+  font-family: 'Baloo 2', sans-serif;
+  font-size: 1.25rem;
+  font-weight: 700;
+  line-height: 1.3;
+
+  margin-bottom: 0.5rem;
+
+  color: ${(props) => props.theme['base-subtitle']};
+`
+export const ProductCardCoffeeDescription = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 130%;
+  text-align: center;
+
+  margin-bottom: 2rem;
+
+  color: ${(props) => props.theme['base-label']};
+`
+export const ProductCardCurrency = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 130%;
+
+  color: ${(props) => props.theme['base-text']};
+`
+export const ProductCardPrice = styled.span`
+  font-family: 'Baloo 2', sans-serif;
+  font-size: 1.5rem;
+  font-weight: 800;
+  line-height: 130%;
+  margin-left: 0.25rem;
+
+  color: ${(props) => props.theme['base-text']};
 `
